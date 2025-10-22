034cacec5337ff9b758bad22c042949d8691d897
Benjamin Cisneros Barraza <cisnerosbarraza@gmail.com>
2025-10-22 07:54:51 -0600

    chore: initial springboot application

---
+++ backend/app/src/main/java/mx/tec/mna/adcs/team4/backend/App.java
@@ -0,0 +1,11 @@
+package mx.tec.mna.adcs.team4.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
